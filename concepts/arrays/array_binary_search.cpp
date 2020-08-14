96c268d433fedad83fc40364a8915967d8938cf7
Parveen Kumar <parveen.kumar6@gmail.com>
2020-08-13 20:54:25 -0700

    cpp

--- concepts/arrays/array_binary_search.cpp
+++ concepts/arrays/array_binary_search.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class BinarySearch {
 public:
 	int binarySearchI(vector<int>& nums, int target);
+	int binarySearchII(vector<int>& nums, int target);
 };
 
 int BinarySearch::binarySearchI(vector<int>& nums, int target) {
@@ -23,14 +24,34 @@ int BinarySearch::binarySearchI(vector<int>& nums, int target) {
 			right = mid - 1;
 		}
 	}
-	return - 1;
+	return -1;
 }
 
+int BinarySearch::binarySearchII(vector<int>& nums, int target) {
+	if (nums.size() == 0)
+		return -1;
+	int left = 0;
+	int right = nums.size();
+	while (left < right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) return mid;
+		else if (nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid;
+		}
+	}
+	cout << "Outside while loop " << endl;
+	if (left != nums.size() && nums[left] == target) return left;
+	return -1;
+}
 
 int main() {
 	BinarySearch bs;
 	vector<int> v {1, 3, 4, 7, 10, 15, 20};
 	int target = 10;
-	cout << bs.binarySearchI(v, target) << endl;
+	cout << "BinarySearch Pattern I " << bs.binarySearchI(v, target) << endl;
+	cout << "BinarySearch Pattern II " << bs.binarySearchII(v, target) << endl;
+
 	return 0;
 }
