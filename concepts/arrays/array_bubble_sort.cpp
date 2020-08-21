6d6161851eb01b784d9e3e6aa5fca792446f2551
Parveen Kumar <parveen.kumar6@gmail.com>
2020-08-20 22:35:48 -0700

    cpp

--- concepts/arrays/array_bubble_sort.cpp
+++ concepts/arrays/array_bubble_sort.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
-void bubble_sort(int[] arr) {
-
+void bubble_sort(int arr[], int n) {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < (n - i); j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
 }
 
 int main() {
+	int arr[] = { 2, 6, 4, 3, 7, 1, 9};
+	int n = sizeof(arr) / sizeof(int);
+	bubble_sort(arr, n);
+	for (int i : arr) {
+		cout << i << " ";
+	}
+	cout << endl;
 	return 0;
 }
