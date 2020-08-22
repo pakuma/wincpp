734e1fa56fb111bd265d0375c1acc6c8c5eadea0
Parveen Kumar <parveen.kumar6@gmail.com>
2020-08-21 22:54:01 -0700

    cpp

--- concepts/arrays/array_insertion_sort.cpp
+++ concepts/arrays/array_insertion_sort.cpp
@@ -7,7 +7,7 @@ void insertion_sort(int arr[], int n) {
 		int current = arr[i];
 
 		int j = i - 1;
-		while (j >= 0 and arr[j] > current) {
+		while (j >= 0 && arr[j] > current) {
 			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
