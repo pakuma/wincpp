b91525fe4a4d4de9fc9312d837017893f5d195f7
Parveen Kumar <parveen.kumar6@gmail.com>
2020-08-24 23:22:24 -0700

    cpp

--- concepts/arrays/array_inbuiltsort_stl.cpp
+++ concepts/arrays/array_inbuiltsort_stl.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main() {
+	int arr[] = {4, 8, 5, 7, 9, 1, 3, 2};
+	int n = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << ",";
+	}
+	cout << endl;
+	cout << n << endl;
+
+	sort(arr, arr + n);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << ",";
+	}
 
 }
