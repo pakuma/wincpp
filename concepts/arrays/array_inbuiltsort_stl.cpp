ba8b30b2765e1d5fc83bc9f4748241015f6a53a0
Parveen Kumar <parveen.kumar6@gmail.com>
2020-08-24 23:33:56 -0700

    cpp

--- concepts/arrays/array_inbuiltsort_stl.cpp
+++ concepts/arrays/array_inbuiltsort_stl.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+//Define A Comparator Function
+bool compare(int a, int b) {
+	//let's check hot many time this compare function is called in sort function
+	cout << "Compare : " << a << " and " << b << endl;
+	return a > b;
+}
+
 int main() {
 	int arr[] = {4, 8, 5, 7, 9, 1, 3, 2};
 	int n = sizeof(arr) / sizeof(int);
@@ -13,8 +20,15 @@ int main() {
 	cout << endl;
 	cout << n << endl;
 
+	//Simple Sort -- increasing order
 	sort(arr, arr + n);
 
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << ",";
+	}
+	cout << endl;
+	//Sort using compare function -- decreasing order
+	sort(arr, arr + n, compare);
 	for (int i = 0; i < n; i++) {
 		cout << arr[i] << ",";
 	}
