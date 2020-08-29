752e027350f65b0c54fb456f5c29d9db7e6dfee6
Parveen Kumar <parveen.kumar6@gmail.com>
2020-08-28 22:36:26 -0700

    kadane's algo

---
+++ concepts/arrays/array_maximum_subarray_kadane.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int arr[] = { -4, 1, 3, -2, 16, 2, -8, -9, 4};
+	int n = sizeof(arr) / sizeof(int);
+
+	int maxSum = arr[0];
+	int currSum = arr[0];
+
+	for (int i = 1; i < n; i++) {
+		currSum = max(arr[i], arr[i] + currSum);
+		maxSum = max(currSum, maxSum);
+	}
+	cout << "Maximum Sum : " << maxSum << "\n";
+}
